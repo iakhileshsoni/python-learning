acf3e05b1ab1f16497ffd83c682097104c5d7428
Akhilesh <akhilesh13.1995@gmail.com>
2025-02-16 15:50:12 +0530

    My first commit

---
+++ intro_python.py
@@ -0,0 +1,35 @@
+# Single line comment
+propose1 = "I love you"
+propose2 = 'I love you'
+
+"""
+ Multi  
+ line 
+ comment
+"""
+propose3 = """
+        I 
+          Love 
+        You
+    """
+propose4 = '''
+        I 
+          Love 
+        You
+    '''
+print(propose1)
+print(propose2)
+
+print(propose3)
+print(propose4)
+
+
+# Syntax- Indentation in Python
+message = 'love'
+if message == "love":
+  print("My love")
+
+
+# Variable in Python
+a = 10
+print(a)
